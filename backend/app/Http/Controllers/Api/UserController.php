49f70660ad1ab7c6a886fe6a19a9788b3efde498
AlexMadrigalAlriols <alex.madrigal.alriols25005@gmail.com>
2023-02-03 17:26:51 +0100

    UserEdit Function

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Colleges;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -81,6 +82,36 @@ class UserController extends Controller
                 "msg" => "Usuario no registrado",
             ]);
         }
+    }
+
+    public function update(Request $request) {
+        $user = $request->user();
+
+        $request->validate([
+            'id_teacher' => 'required'
+        ]);
+        
+        $set_clause_parts = [];
+        foreach($request->all() as $key => $value) {
+            if($key != "id_teacher") {
+                $set_clause_parts[] = "{$key}='{$value}'";
+            }
+        }
+                
+        $set_clause = implode(', ', $set_clause_parts);
+        $rows_affected = DB::update('UPDATE teachers SET '.$set_clause.' where id_teacher = ?', [$request->id_teacher]);
+
+        if($rows_affected > 0) {
+            return response()->json([
+                "status" => 200,
+                "msg"   => "Se ha actualizado con exito",
+            ]);
+        }
+
+        return response()->json([
+            "status" => 300,
+            "msg"   => "No se ha encontrado el profesor para actualizar",
+        ]);
     }
     //Esta funcion muestra el perfil del usuario
     public function getDetails(Request $request) {
