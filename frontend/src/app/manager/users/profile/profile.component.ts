fd94ce7becc2fff8d4429da8a99352a7ccd3da10
Alex Madrigal Alriols <73166221+AlexMadrigalAlriols@users.noreply.github.com>
2023-02-11 17:44:03 +0100

    Profile Update

--- frontend/src/app/manager/users/profile/profile.component.ts
+++ frontend/src/app/manager/users/profile/profile.component.ts
@@ -12,6 +12,7 @@ export class ProfileComponent implements OnInit {
   dataLoaded!: Promise<boolean>;
   user_data: any;
   id_profile = "";
+  selfProfile: boolean = false;
 
   constructor(private authService: AuthService, public userService: UserService,  private readonly route: ActivatedRoute ) { }
 
@@ -20,13 +21,20 @@ export class ProfileComponent implements OnInit {
       this.id_profile = params['id']; // (+) converts string 'id' to a number
     });
 
+    this.selfProfile = this.id_profile == String(localStorage.getItem('id'));
     this.userService.getUserDetails(String(this.id_profile)).subscribe((response: any) => {
       if(response.status == 200 && response.data) {
         this.user_data = response.data;
 
-        this.dataLoaded = Promise.resolve(true);
-      } else {
-        this.authService.logout();
+        this.userService.getCourses(String(localStorage.getItem('id'))).subscribe((courses: any) => {
+          if(courses.status == 200) {
+            this.user_data.courses = courses.data;
+    
+            this.dataLoaded = Promise.resolve(true);
+          } else {
+            this.authService.logout();
+          }
+        });
       }
     });
   }
