bdd0ce403c65ab814f81333c0f3ec27899319151
Rulexloko13 <rubenlechosa@gmail.com>
2023-02-28 20:16:34 +0100

    dsg

--- frontend/src/app/manager/courses/tasks/tasks.component.ts
+++ frontend/src/app/manager/courses/tasks/tasks.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CourseService } from 'src/app/services/course.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
+  dataLoaded!: Promise<boolean>;
+  user_data: any;
+  id_course?: number;
+
+
+constructor(private authService: AuthService, private userService: UserService, private route: ActivatedRoute, private courseService: CourseService) { }
+
+
+
+ngOnInit(): void {
+
+  this.route.params.subscribe(params => {
+    this.id_course = params['id']; // (+) converts string 'id' to a number
+  });
+
+
+  this.userService.getUserDetails(String(localStorage.getItem('id'))).subscribe((response: any) => {
+    if(response.status == 200 && response.data) {
+      this.user_data = response.data;
+      
+      this.authService.checkPermissions(this.user_data.role);
+      this.dataLoaded = Promise.resolve(true);
+
+    } else {
+      this.authService.logout();
+    }
+  });
+}
 
 }
