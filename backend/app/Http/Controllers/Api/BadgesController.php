9e7c0270569e20a74a4e743ef9acf6b715e3a021
AlexMadrigalAlriols <alex.madrigal.alriols25005@gmail.com>
2023-05-26 14:04:18 +0200

    Badge Lvl

--- backend/app/Http/Controllers/Api/BadgesController.php
+++ backend/app/Http/Controllers/Api/BadgesController.php
@@ -33,18 +33,18 @@ class BadgesController extends Controller
                             if($user_badge->level < 5) {
                                 $user_badge->level++;
                                 $user_badge->actualExp = ($user_badge->actualExp - $user_badge->expToNextLvl);
-                                $user_badge->expToNextLvl += 1100;
+                                $user_badge->expToNextLvl += 500;
                             }
                         }
                         
                         $user_badge->save();
                     } else {
                         $user_badge = new Badges();
-                        $user_badge->level = ($badge["value"] >= 1000 ? 1 : 0);
+                        $user_badge->level = ($badge["value"] >= 500 ? 1 : 0);
                         $user_badge->id_user = $request["id_user"];
                         $user_badge->type = $badge["name"];
-                        $user_badge->actualExp = ($badge["value"] >= 1000 ? 0 : $badge["value"]);
-                        $user_badge->expToNextLvl = ($badge["value"] >= 1000 ? 2100 : 1000);
+                        $user_badge->actualExp = ($badge["value"] >= 500 ? 0 : $badge["value"]);
+                        $user_badge->expToNextLvl = ($badge["value"] >= 500 ? 1000 : 500);
     
                         $user_badge->save();
                     }
